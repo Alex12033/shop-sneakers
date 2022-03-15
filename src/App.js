c26cd5d0c747e40c95fc7a9587308767086eba27
Aleksandr <aleksandrignashov42@gmail.com>
2022-03-15 15:09:45 +0200

    fix html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,171 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2>
+            Cart{" "}
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </h2>
+          <div className="scroll">
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+            <div className="cartItem">
+              <img
+                width={70}
+                height={70}
+                src="/img/sneakers/1.jpg"
+                alt="Sneakers"
+              />
+              <div className="titlePrice">
+                <p>Man sneakers Nike Blazer Mid Seude</p>
+                <b>12 999$</b>
+              </div>
+              <img
+                className="removeBtn"
+                src="/img/btn-remove.svg"
+                alt="Remove"
+              />
+            </div>
+          </div>
+          <div className="items">
+            <ul className="cartTotalBlock">
+              <li>
+                <span>Total:</span>
+                <div></div>
+                <b>21 400$</b>
+              </li>
+              <li>
+                <span>Tax 5%:</span>
+                <div></div>
+                <b>1040$</b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Check order{" "}
+              <img className="arrow" src="/img/arrow.svg" alt="arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
+
       <header>
         <div className="headerLeft">
           <img width="40" height="40" src="/img/logo.png" alt="logo" />
@@ -133,6 +298,98 @@ function App() {
           </div>
           <div className="card">
             <img width="133" height="122" src="/img/sneakers/6.jpg" alt="" />
+            <h5>Man sneakers Nike Blazer Mid Seude</h5>
+            <div className="cardBottom">
+              <div className="priceCard">
+                <span>Price:</span>
+                <b>12 999 $</b>
+                <div>
+                  <button>
+                    <img
+                      width="11"
+                      height="11"
+                      src="/img/plus.svg"
+                      alt="plus"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="unliked" />
+            </div>
+            <img width="133" height="122" src="/img/sneakers/7.jpg" alt="" />
+            <h5>Man sneakers Nike Blazer Mid Seude</h5>
+            <div className="cardBottom">
+              <div className="priceCard">
+                <span>Price:</span>
+                <b>12 999 $</b>
+                <div>
+                  <button>
+                    <img
+                      width="11"
+                      height="11"
+                      src="/img/plus.svg"
+                      alt="plus"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="unliked" />
+            </div>
+            <img width="133" height="122" src="/img/sneakers/8.jpg" alt="" />
+            <h5>Man sneakers Nike Blazer Mid Seude</h5>
+            <div className="cardBottom">
+              <div className="priceCard">
+                <span>Price:</span>
+                <b>12 999 $</b>
+                <div>
+                  <button>
+                    <img
+                      width="11"
+                      height="11"
+                      src="/img/plus.svg"
+                      alt="plus"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="unliked" />
+            </div>
+            <img width="133" height="122" src="/img/sneakers/9.jpg" alt="" />
+            <h5>Man sneakers Nike Blazer Mid Seude</h5>
+            <div className="cardBottom">
+              <div className="priceCard">
+                <span>Price:</span>
+                <b>12 999 $</b>
+                <div>
+                  <button>
+                    <img
+                      width="11"
+                      height="11"
+                      src="/img/plus.svg"
+                      alt="plus"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="favorite">
+              <img src="/img/unliked.svg" alt="unliked" />
+            </div>
+            <img width="133" height="122" src="/img/sneakers/10.jpg" alt="" />
             <h5>Man sneakers Nike Blazer Mid Seude</h5>
             <div className="cardBottom">
               <div className="priceCard">
