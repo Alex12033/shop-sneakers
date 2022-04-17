fa45080a5ce4aeabf0a8396d11a56c0a0ed1ef8f
Aleksandr <aleksandrignashov42@gmail.com>
2022-04-17 20:27:38 +0300

    was added empty favorites block in favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Card from "../components/Card";
+import EmptyFavorits from "../components/EmptyFavorits";
 
 function Favorites({ searchValue, setSearchValue, onChangeSearchInput }) {
   const [favorites, setFavorites] = useState([]);
 
   useState(() => {
-    axios
-      .get("http://localhost:8000/favorites")
-      .then((res) => {
-        setFavorites(res.data);
-      });
+    axios.get("http://localhost:8000/favorites").then((res) => {
+      setFavorites(res.data);
+    });
   }, []);
 
   const onRemoveFavorites = (obj) => {
-    axios.delete(
-      `http://localhost:8000/favorites/${obj.id}`
-    );
+    axios.delete(`http://localhost:8000/favorites/${obj.id}`);
     setFavorites((prev) => prev.filter((item) => item.id !== obj.id));
   };
 
@@ -42,17 +39,21 @@ function Favorites({ searchValue, setSearchValue, onChangeSearchInput }) {
         </div>
       </div>
       <div className="sneakers">
-        {favorites.map((obj) => (
-          <Card
-            onRemoveFavorites={onRemoveFavorites}
-            heartFavorites={false}
-            id={obj.id}
-            name={obj.name}
-            price={obj.price}
-            key={obj.id}
-            src={obj.src}
-          />
-        ))}
+        {favorites.length === 0 ? (
+          <EmptyFavorits />
+        ) : (
+          favorites.map((obj) => (
+            <Card
+              onRemoveFavorites={onRemoveFavorites}
+              heartFavorites={false}
+              id={obj.id}
+              name={obj.name}
+              price={obj.price}
+              key={obj.id}
+              src={obj.src}
+            />
+          ))
+        )}
       </div>
     </div>
   );
