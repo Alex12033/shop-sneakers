690e0b81079df5e1de00a19e81baad87cbc1b2db
Aleksandr <aleksandrignashov42@gmail.com>
2022-04-27 10:30:46 +0300

    for heroku

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('./public/db.json');
-// const middlewares = jsonServer.defaults({
-//   static: './build'
-// });
-// const PORT = process.env.PORT || 8000;
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(PORT, () => {
-//   console.log('Server is running');
-// });
-
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
 });
+const PORT = process.env.PORT || 8000;
+server.use(middlewares);
+server.use(router);
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+server.listen(PORT, () => {
+  console.log('Server is running');
 });
-
-app.listen(process.env.PORT || 8000);
