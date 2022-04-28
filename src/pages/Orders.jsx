7c959bbde69150e28367c2e0748532e60338f1f0
Aleksandr <aleksandrignashov42@gmail.com>
2022-04-28 20:29:40 +0300

    fix resolution on large screen and fix title search rch rch in orders

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,15 +1,27 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+
+import React, { useEffect, useState, useContext } from "react";
+
 import Card from "../components/Card";
 import Notice from "../components/Notice";
+
+import style from "./Home.module.scss";
+
+import AppContext from "../components/context";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const { searchValue } = useContext(AppContext);
+  const { setSearchValue } = useContext(AppContext);
+  const { onChangeSearchInput } = useContext(AppContext);
+
   useEffect(() => {
     (async () => {
-      const { data } = await axios.get("https://sneakers-course.herokuapp.com/api/orders");
+      const { data } = await axios.get(
+        "https://sneakers-course.herokuapp.com/api/orders"
+      );
       setOrders(data.reduce((prev, obj) => [...prev, ...obj], []));
       setTimeout(() => {
         //it wrong! LATER i fix it!
@@ -21,7 +33,32 @@ const Orders = () => {
   return (
     <div className="content">
       <div className="title-search">
-        <h1>"Orders"</h1>
+        <div className={style.title_search}>
+          <h1>
+            {searchValue ? `Search request: ${searchValue}` : "Orders"}
+          </h1>
+          <div className={style.search_block}>
+            <img
+              className={style.searchImage}
+              src="/img/search.svg"
+              alt="search"
+            />
+            {searchValue && (
+              <img
+                onClick={() => setSearchValue("")}
+                className={style.remove}
+                src="/img/btn-remove.svg"
+                alt="remove"
+              />
+            )}
+            <input
+              className={style.searchHome}
+              onChange={onChangeSearchInput}
+              value={searchValue}
+              placeholder="Search..."
+            />
+          </div>
+        </div>
       </div>
       <div className="sneakers">
         {orders.length === 0 ? (
