e7605576b236c0921077b224924bc01dea3e286d
Aleksandr <aleksandrignashov42@gmail.com>
2022-05-05 11:27:30 +0300

    fix reload form login

--- src/Auth/SignIn.js
+++ src/Auth/SignIn.js
@@ -22,7 +22,7 @@ export const SignIn = () => {
       alert("Fill in all the fields");
     } else {
       await axios.post("https://sneakers-course.herokuapp.com/api/users", auth);
-      window.location.href = "https://www.google.com/search?q=window.location.href+heroku+app&sxsrf=ALiCzsacYEqkv5QHnfaRyT5jIRg30GYCrQ%3A1651738619295&ei=-4dzYuPZEcGOrwTz3LbAAQ&oq=window.location.href+heroku&gs_lcp=Cgdnd3Mtd2l6EAMYADIFCCEQoAEyBQghEKABOgcIIxDqAhAnOgQIIxAnOgQIABBDOhEILhCABBCxAxCDARDHARCjAjoOCC4QgAQQsQMQxwEQ0QM6CAgAEIAEELEDOgsIABCABBCxAxCDAToHCAAQsQMQQzoKCAAQgAQQhwIQFDoFCAAQgAQ6BQgAEMsBOgYIABAWEB5KBAhBGABKBAhGGABQ4x1YoXZgp4cBaAFwAXgAgAGaAYgBqw6SAQQxMi42mAEAoAEBsAEKwAEB&sclient=gws-wiz"; // need for renew component and get data in login form
+      window.location.href = "https://sneakers-course.herokuapp.com/LoginForm"; // need for renew component and get data in login form
     }
     setLogin("");
     setPassword("");
